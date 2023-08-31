f700715f15d27696fcec6e54e2540f2c2da57546
Mihir Pratap Sah <smihirp1@gmail.com>
2023-08-31 09:41:21 +0530

    ...

--- lib/widgets/new_udhaari.dart
+++ lib/widgets/new_udhaari.dart
@@ -10,15 +10,31 @@ class NewUdhaari extends StatefulWidget {
 }
 
 class _NewUdhaariState extends State<NewUdhaari> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: InputDecoration(label: Text("Title")),
+            decoration: const InputDecoration(label: Text("Title")),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_enteredTitle);
+                  },
+                  child: const Text("Save Expense"))
+            ],
           )
         ],
       ),
