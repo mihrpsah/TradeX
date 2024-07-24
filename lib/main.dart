fb0cd7f9f136a3a912d520babac2063c9d67de6d
Mihir Pratap Sah <smihirp1@gmail.com>
2024-07-24 18:42:01 +0530

    ...

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:udhaari_tracker/widgets/udhaaris.dart';
-
-void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const Udhaaris(),
-  ));
-}
